01f7a26819dc2ffc495e581155dbd7379978f531
William Rory Kronmiller <wrk961@gmail.com>
2017-01-07 12:12:51 -0500

    finish

--- build.sbt
+++ build.sbt
@@ -5,3 +5,4 @@ scalaVersion := "2.11.8"
 name := "WikipediaWordCount"
 
 libraryDependencies += "com.typesafe" % "config" % "1.3.0"
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.1.0"
--- src/main/scala/net/kronmiller/william/wikiwordcount/Core.scala
+++ src/main/scala/net/kronmiller/william/wikiwordcount/Core.scala
@@ -1,7 +1,55 @@
 package net.kronmiller.william.wikiwordcount
 
+import scala.io.Source
+import java.io.File
+import scala.collection.parallel.immutable.ParSeq
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object Parser {
+  private val textRegex = "\\<text xml\\:space=\"preserve\"\\>(.*)\\</text\\>".r
+  private val wordRegex = "[a-zA-Z\\']+"
+  def getWords(source: String) = {
+    textRegex
+      .findAllIn(source)
+      .toList
+      .par
+      .map(text => {
+        val textRegex(isolatedText) = text
+        isolatedText
+      })
+      .flatMap(_.split("\\s+").toList)
+      .filter(_.matches(wordRegex))
+      .map(_.replace("''", ""))
+      .filter(_.length > 0)
+  }
+}
+
+class XMLLoader(xmlPath: String) {
+  def load = {
+    Source
+      .fromFile(new File(xmlPath))
+      .getLines
+      .mkString(" ")
+  }
+}
+
 object Core {
+  val NUM_SLICES = 30
   def main(args: Array[String]) {
-    println("hello world")
+    val Array(xmlPath) = args
+    println("Loading text")
+    val loader = new XMLLoader(xmlPath)
+    println("Flatmapping words")
+    val parWords: List[String] = Parser.getWords(loader.load).toList
+    println("Starting spark")
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("wikiwordcount")
+    val sc = new SparkContext(sparkConf)
+
+    sc.parallelize[String](parWords, NUM_SLICES).map((_, 1)).reduceByKey(_ + _).saveAsTextFile("counts")
+
+    sc.stop
   }
 }
