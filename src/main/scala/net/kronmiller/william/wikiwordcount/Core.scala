2de50ebc18d4256e0209a06416db6f1ae629fd3e
William Rory Kronmiller <wrk961@gmail.com>
2017-01-07 12:16:33 -0500

    Use spark not parseq

--- src/main/scala/net/kronmiller/william/wikiwordcount/Core.scala
+++ src/main/scala/net/kronmiller/william/wikiwordcount/Core.scala
@@ -7,25 +7,6 @@ import scala.collection.parallel.immutable.ParSeq
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
-object Parser {
-  private val textRegex = "\\<text xml\\:space=\"preserve\"\\>(.*)\\</text\\>".r
-  private val wordRegex = "[a-zA-Z\\']+"
-  def getWords(source: String) = {
-    textRegex
-      .findAllIn(source)
-      .toList
-      .par
-      .map(text => {
-        val textRegex(isolatedText) = text
-        isolatedText
-      })
-      .flatMap(_.split("\\s+").toList)
-      .filter(_.matches(wordRegex))
-      .map(_.replace("''", ""))
-      .filter(_.length > 0)
-  }
-}
-
 class XMLLoader(xmlPath: String) {
   def load = {
     Source
@@ -37,18 +18,35 @@ class XMLLoader(xmlPath: String) {
 
 object Core {
   val NUM_SLICES = 30
+  private val textRegex = "\\<text xml\\:space=\"preserve\"\\>(.*)\\</text\\>".r
+  private val wordRegex = "[a-zA-Z\\']+"
+  def getWords(source: String) = {
+    textRegex
+      .findAllIn(source)
+      .toList
+  }
+
   def main(args: Array[String]) {
     val Array(xmlPath) = args
     println("Loading text")
     val loader = new XMLLoader(xmlPath)
     println("Flatmapping words")
-    val parWords: List[String] = Parser.getWords(loader.load).toList
+    val parWords: List[String] = getWords(loader.load)
     println("Starting spark")
 
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("wikiwordcount")
     val sc = new SparkContext(sparkConf)
 
-    sc.parallelize[String](parWords, NUM_SLICES).map((_, 1)).reduceByKey(_ + _).saveAsTextFile("counts")
+    sc.parallelize[String](parWords, NUM_SLICES)
+      .map(text => {
+        val textRegex(isolatedText) = text
+        isolatedText
+      })
+      .flatMap(_.split("\\s+").toList)
+      .filter(_.matches(wordRegex))
+      .map(_.replace("''", ""))
+      .filter(_.length > 0)
+      .map((_, 1)).reduceByKey(_ + _).saveAsTextFile("counts")
 
     sc.stop
   }
